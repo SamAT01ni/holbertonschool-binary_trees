b348231cdcc69c3b3a1c4492973e11cbcb757345
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-04-03 08:23:02 +0000

    fixes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,9 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
-
 	if (tree == NULL)
 		return (0);
-	
+
 	return ((int)binary_tree_height(tree->left) -
 			(int)binary_tree_height(tree->right));
 }
