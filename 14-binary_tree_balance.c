bad89a6d9927f20d3af792193d25c9a926d5fd27
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-04-03 08:27:43 +0000

    added fn in

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance
+ * binary_tree_height - measures height of the tree
+ * @tree: pointer to root
+ *
+ * Return: height or 0 if null
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
+
+	if (left > right)
+		return (left);
+	else
+		return (right);
+}
+
+/**
+ * binary_tree_balance - balances tree
  * @tree: tree
  *
  * Return: balance factor or 0
